1b24998fa6879ec72076f2dd187fd42f7a984324
free-trial-only <63915415+free-trial-only@users.noreply.github.com>
2020-04-18 21:02:11 +0530

    Update 99bottles.py

--- 99bottles.py
+++ 99bottles.py
@@ -5,6 +5,7 @@
 
 bottles=99
 while bottles>1:
-  print bottles, "bottles of beer on the wall,", bottles, "bottles of beer on the wall,\n", "Take one down and pass it around,", bottles-1, "bottles of beer on the wall."
+  print bottles, "bottles of beer on the wall,", bottles, "bottles of beer.,\n", "Take one down and pass it around,", bottles-1, "bottles of beer on the wall."
   bottles=bottles-1
 print "1 bottle of beer on the wall, 1 bottle of beer.\n Take one down and pass it around, no more bottles of beer on the wall.\n No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall."
+#A word of caution(DO NOT SKIP IT!)(I SAID READ IT!!!): Drinking is injurious to health. All bottles shown in the code do not promote drinking. Its a nursery rhyme, dont take it too seriously. I am really serious af. Also you can never try weed no matter what the world tells you, but once in a lifetime sounds good. For more theories on how marijuana is bliss, text me on github. I am open to queries and feedbacks.
