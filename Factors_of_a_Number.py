5289a75d9b839deddb2652ba927795905bb7de94
Koushik <72181822+koushik798@users.noreply.github.com>
2020-10-01 11:52:17 +0530

    Factors _of_number_explained.py

--- Factors_of_a_Number.py
+++ Factors_of_a_Number.py
@@ -1,4 +1,7 @@
+# python code to find factors of a number
 n=int(input())
 for i in range(1,n+1):
     if n%i == 0 :
         print(i)
+# n is the input 
+''' The above program prints all the factors of number that has been given as input'''
